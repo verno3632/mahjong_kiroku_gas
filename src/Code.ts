0f0e76003220b815b74aca033d4a5c2bad2a89d8
verno3632 <verno3632@pixiv.com>
2019-07-07 23:44:29 +0900

    lint

--- src/Code.ts
+++ src/Code.ts
@@ -88,13 +88,13 @@ const targetSheet = "season4";
 const userRowNum = 1;
 const userStartColumnNum = 4;
 
-function getMembers(){
+function getMembers() {
   const activeSpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
   const sheet = activeSpreadSheet.getSheetByName(targetSheet);
   const lastColumn = sheet.getLastColumn();
 
   const nameRange = sheet.getRange(userRowNum, userStartColumnNum, userRowNum, lastColumn);
-  let array = [];
+  const array = [];
   for (let index = userStartColumnNum; index < lastColumn; index++) {
     array.push(sheet.getRange(userRowNum, index).getValue());
   }
