0302686127ba181211ae22c9b7562228efad0ae8
verno3632 <verno3632@pixiv.com>
2019-07-08 00:34:20 +0900

    deployコケさせる

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -4,5 +4,5 @@ set -e
 
 echo $CLASPRC_JSON > ~/.clasprc.json
 sed -e "s/SCRIPT_ID/$SCRIPT_ID/g" .clasp.json.template > .clasp.json
-npx @google/clasp push | grep errors || exit 1
-npx @google/clasp deploy --description `git rev-parse HEAD` || exit 1
+npx @google/clasp push | grep errors && exit 1
+npx @google/clasp deploy --description `git rev-parse HEAD` && exit 1
