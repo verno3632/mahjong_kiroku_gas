b8f204344e944f5e7c114d8822b5cb09034939ff
verno3632 <verno3632@pixiv.com>
2019-07-08 00:40:46 +0900

    revertを戻す

--- src/Code.ts
+++ src/Code.ts
@@ -104,8 +104,10 @@ function getMembers() {
 }
 
 function doGet() {
+  const members = getMembers().sort();
+
   const output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
-  output.setContent(JSON.stringify(getMembers().sort().reverse()));
+  output.setContent(JSON.stringify(members));
   return output;
 }
